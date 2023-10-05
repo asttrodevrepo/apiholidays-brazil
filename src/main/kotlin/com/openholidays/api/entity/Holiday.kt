e7d410b52bcc12c506b487ef32b65665bd72ff5f
asttro.dev <asttro.dev@gmail.com>
2023-10-04 21:36:11 -0300

    feat(Holiday): add entity holiday

---
+++ src/main/kotlin/com/openholidays/api/entity/Holiday.kt
@@ -0,0 +1,27 @@
+package com.openholidays.api.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import java.io.Serializable
+import java.time.LocalDate
+
+@Entity
+class Holiday (
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private val id: Int,
+    private val name: String = "",
+    private val holidayDate: LocalDate,
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private val city: City? = null,
+    @ManyToOne
+    @JoinColumn(name = "state_id")
+    private val state: State? = null,
+
+): AuditModel(), Serializable
